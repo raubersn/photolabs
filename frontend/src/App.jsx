0128def1107b5641925d311d48a5aa64ba9bb26d
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-09-15 17:00:38 -0700

    Connect the Favourite Icon and Modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import HomeRoute from 'routes/HomeRoute';
-import PhotoDetailsModal from 'routes/PhotoDetailsModal';
-import './App.scss';
+import React, { useState } from "react";
+import HomeRoute from "routes/HomeRoute";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
+import "./App.scss";
 
-import topics from 'mocks/topics';
-import photos from 'mocks/photos';
+import topics from "mocks/topics";
+import photos from "mocks/photos";
 
 const App = () => {
   const [getModal, setModal] = useState(null);
@@ -13,21 +13,34 @@ const App = () => {
     setModal(photo);
   };
 
-  const [getFavourites, setFavourites] = useState(['1', '3', '4', '7', '8']);
+  const [getFavourites, setFavourites] = useState(["1", "3", "4", "7", "8"]);
 
   const handleFavourites = (favID, selected) => {
     if (selected) {
       setFavourites([...getFavourites, favID]);
-    }
-    else {
-      setFavourites(getFavourites.filter(x => x !== favID));
+    } else {
+      setFavourites(getFavourites.filter((x) => x !== favID));
     }
   };
 
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos} photoDetails={showModal} favourites={getFavourites} handleFavourites={handleFavourites}/>
-      {getModal && <PhotoDetailsModal photo={getModal} closeModal={() => showModal(null)} photoDetails={showModal} favourites={getFavourites} handleFavourites={handleFavourites}/>}
+      <HomeRoute
+        topics={topics}
+        photos={photos}
+        photoDetails={showModal}
+        favourites={getFavourites}
+        handleFavourites={handleFavourites}
+      />
+      {getModal && (
+        <PhotoDetailsModal
+          photo={getModal}
+          closeModal={() => showModal(null)}
+          photoDetails={showModal}
+          favourites={getFavourites}
+          handleFavourites={handleFavourites}
+        />
+      )}
     </div>
   );
 };
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
-import '../styles/PhotoDetailsModal.scss'
-import closeSymbol from '../assets/closeSymbol.svg';
-import PhotoListItem from 'components/PhotoListItem';
-import PhotoList from 'components/PhotoList';
+import React from "react";
+import "../styles/PhotoDetailsModal.scss";
+import closeSymbol from "../assets/closeSymbol.svg";
+import PhotoListItem from "components/PhotoListItem";
+import PhotoList from "components/PhotoList";
 
-const PhotoDetailsModal = ({ photo, closeModal, photoDetails, favourites, handleFavourites }) => {
+const PhotoDetailsModal = ({
+  photo,
+  closeModal,
+  photoDetails,
+  favourites,
+  handleFavourites,
+}) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={closeModal}>
+      <button
+        className="photo-details-modal__close-button"
+        onClick={closeModal}
+      >
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      <PhotoListItem modal={true} photo={photo} selected={favourites.filter(x => x === photo.id).length > 0} />
-      <label className='photo-details-modal__header'>Similar Photos</label>
-      <PhotoList photos={photo.similar_photos} favourites={favourites} handleFavourites={handleFavourites} photoDetails={photoDetails}/>
+      <PhotoListItem
+        modal={true}
+        photo={photo}
+        selected={favourites.filter((x) => x === photo.id).length > 0}
+        handleFavourites={handleFavourites}
+        favID={photo.id}
+      />
+      <label className="photo-details-modal__header">Similar Photos</label>
+      <PhotoList
+        photos={photo.similar_photos}
+        favourites={favourites}
+        handleFavourites={handleFavourites}
+        photoDetails={photoDetails}
+      />
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
