6561e736fd50890ca275fcb7e8cc3aecfbc6500b
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-09-18 18:00:43 -0700

    Replace the Switch statements of useApplicationData with an Object

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -9,40 +9,50 @@ export const ACTIONS = {
   DISPLAY_PHOTO_DETAILS: "DISPLAY_PHOTO_DETAILS",
 };
 
+const reducers = {
+  FAV_PHOTO_ADDED(state, action) {
+    return {
+      favourites: [...state.favourites, action.payload],
+      modalPhoto: state.modalPhoto ? { ...state.modalPhoto } : null,
+    };
+  },
+  FAV_PHOTO_REMOVED(state, action) {
+    return {
+      favourites: state.favourites.filter((x) => x !== action.payload),
+      modalPhoto: state.modalPhoto ? { ...state.modalPhoto } : null,
+    };
+  },
+  SET_PHOTO_DATA(state, action) {
+    return {
+      /* insert logic */
+    };
+  },
+  SET_TOPIC_DATA(state, action) {
+    return {
+      /* insert logic */
+    };
+  },
+  SELECT_PHOTO(state, action) {
+    return {
+      favourites: [...state.favourites],
+      modalPhoto: action.payload,
+    };
+  },
+  DISPLAY_PHOTO_DETAILS(state, action) {
+    return {
+      /* insert logic */
+    };
+  },
+};
+
 export const useApplicationData = (initial) => {
   const [state, dispatch] = useReducer((state, action) => {
-    switch (action.type) {
-      case ACTIONS.FAV_PHOTO_ADDED:
-        return {
-          favourites: [...state.favourites, action.payload],
-          modalPhoto: state.modalPhoto ? { ...state.modalPhoto } : null,
-        };
-      case ACTIONS.FAV_PHOTO_REMOVED:
-        return {
-          favourites: state.favourites.filter((x) => x !== action.payload),
-          modalPhoto: state.modalPhoto ? { ...state.modalPhoto } : null,
-        };
-      case ACTIONS.SET_PHOTO_DATA:
-        return {
-          /* insert logic */
-        };
-      case ACTIONS.SET_TOPIC_DATA:
-        return {
-          /* insert logic */
-        };
-      case ACTIONS.SELECT_PHOTO:
-        return {
-          favourites: [...state.favourites],
-          modalPhoto: action.payload,
-        };
-      case ACTIONS.DISPLAY_PHOTO_DETAILS:
-        return {
-          /* insert logic */
-        };
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        );
+    if (reducers[action.type]) {
+      return reducers[action.type](state, action);
+    } else {
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
     }
   }, initial || { favourites: [], modalPhoto: null });
 
